797800f32c6b22456802b06a1124e424bbb5341c
Karl Hiner <karl.hiner@gmail.com>
2023-11-20 18:14:02 -0500

    Work on `chop_dataset.py`. Add `create_label_mapping.py` and `create_slim_metadata.py` scripts. Add `explore_dataset` notebook to evaluate chopped training set generation.

---
+++ create_label_mapping.py
@@ -0,0 +1,103 @@
+# This script searches through all MIDI files in the slimmed dataset to find all unique midi notes.
+# (See `create_slim_metadata.py` for details on the slimmed dataset.)
+#
+# The following metadata output files are generated:
+# * `dataset/note_occurrences_slim.csv`, with the following columns:
+#   - `note`: The MIDI note number.
+#   - `name`: The human-readable name for the note.
+#   - `occurrences`: The number of times the a message with this note value occurs in the slimmed dataset.
+#   - `rank`: The occurrence frequency rank of the note.
+# * `dataset/label_mapping.csv`, with the following columns, generated from the top-5 most common notes.
+#   This is the final note mapping used for training. Other notes are ignored.
+#   - `id`: A unique ID for the note. Used for one-hot encoding.
+#   - `note`: The MIDI note number.
+#   - `name`: The human-readable name for the note.
+
+from mido import MidiFile
+import pandas as pd
+
+# Relevant standard drum MIDI note mappings (there are higher values but they don't occur in the slimmed dataset)):
+# There are two notes in the slimmed dataset with unknown mappings (below this standard range): 22 and 26.
+# See https://computermusicresource.com/GM.Percussion.KeyMap.html and other sources.
+NAME_FOR_NOTE = {
+    35: 'Acoustic Bass Drum',
+    36: 'Bass Drum',
+    37: 'Side Stick',
+    38: 'Acoustic Snare',
+    39: 'Hand Clap',
+    40: 'Electric Snare',
+    41: 'Low Floor Tom',
+    42: 'Closed Hi Hat',
+    43: 'High Floor Tom',
+    44: 'Pedal Hi-Hat',
+    45: 'Low Tom',
+    46: 'Open Hi-Hat',
+    47: 'Low-Mid Tom',
+    48: 'Hi-Mid Tom',
+    49: 'Crash Cymbal 1',
+    50: 'High Tom',
+    51: 'Ride Cymbal 1',
+    52: 'Chinese Cymbal',
+    53: 'Ride Bell',
+    54: 'Tambourine',
+    55: 'Splash Cymbal',
+    56: 'Cowbell',
+    57: 'Crash Cymbal 2',
+    58: 'Vibraslap',
+    59: 'Ride Cymbal 2',
+}
+
+DATASET_DIR = 'dataset/e-gmd-v1.0.0'
+METADATA_PATH = 'dataset/e-gmd-v1.0.0-slim.csv'
+OCCURRENCES_OUT_CSV_PATH = 'dataset/note_occurrences_slim.csv'
+MAPPING_OUT_CSV_PATH = 'dataset/label_mapping.csv'
+
+# Output dataset column names:
+RANK = 'rank'
+NOTE = 'note'
+NAME = 'name'
+OCCURRENCES = 'occurrences'
+
+# Output label mapping column names:
+ID = 'id'
+# NOTE
+# NAME
+
+if __name__ == '__main__':
+    metadata_df = pd.read_csv(METADATA_PATH)
+    all_notes = dict() # Key: MIDI note number, Value: Occurrence count
+    unique_midi_filenames = metadata_df.midi_filename.unique()
+    for midi_filename in unique_midi_filenames:
+        midi_file_path = f'{DATASET_DIR}/{midi_filename}'
+        print(f'Processing {midi_file_path}...')
+        midi_file = MidiFile(midi_file_path)
+        assert(len(midi_file.tracks) == 2) # First track is metadata, then one track of drum notes.
+        for msg in midi_file.tracks[1]:
+            if not hasattr(msg, 'note'):
+                continue # Metadata, program change, etc.
+            all_notes[msg.note] = all_notes.get(msg.note, 0) + 1
+
+    print(f'{METADATA_PATH} has {len(all_notes)} unique notes with the following occurrence counts:')
+    sorted_entries = sorted(all_notes.items(), key=lambda item: item[1], reverse=True)
+    for key, value in sorted_entries:
+        print(f"{key}: {value}")
+
+    print(f'Writing note occurrence metadata to {OCCURRENCES_OUT_CSV_PATH}...')
+    occurrences = {NOTE: [], NAME: [], OCCURRENCES: [], RANK: []}
+    for i, (note, count) in enumerate(sorted_entries):
+        occurrences[NOTE].append(note)
+        occurrences[NAME].append(NAME_FOR_NOTE.get(note, 'Unknown'))
+        occurrences[OCCURRENCES].append(count)
+        occurrences[RANK].append(i)
+
+    occurrences_df = pd.DataFrame(occurrences)
+    occurrences_df.to_csv(OCCURRENCES_OUT_CSV_PATH, index=False)
+
+    mapping = {ID: [], NOTE: [], NAME: []}
+    for i, (note, count) in enumerate(sorted_entries[:5]):
+        mapping[ID].append(i)
+        mapping[NOTE].append(note)
+        mapping[NAME].append(NAME_FOR_NOTE[note])
+
+    mapping_df = pd.DataFrame(mapping)
+    mapping_df.to_csv(MAPPING_OUT_CSV_PATH, index=False)
---
+++ create_slim_metadata.py
@@ -0,0 +1,52 @@
+# This script generates a curated subset of `e-gmd-v1.0.0.csv`.
+# This subset only includes MIDI files from drummer 1, session 1.
+# It also excludes several kits to limit the sound range to "standard"-sounding drum kits.
+
+import pandas as pd
+
+DATASET_DIR = 'dataset/e-gmd-v1.0.0'
+METADATA_PATH = f'{DATASET_DIR}/e-gmd-v1.0.0.csv'
+SLIM_METADATA_OUT_CSV_PATH = 'dataset/e-gmd-v1.0.0-slim.csv'
+SESSION_NAME = 'drummer1/session1' # The session to include in the slimmed dataset.
+
+if __name__ == '__main__':
+    metadata_df = pd.read_csv(METADATA_PATH)
+    original_row_count = len(metadata_df)
+    metadata_df = metadata_df[metadata_df.session == SESSION_NAME]
+    all_kit_names = set(metadata_df.kit_name.dropna().unique())
+    include_kit_names = set([
+        'Acoustic Kit', 'Studio (Live Room)', 'Classic Rock', 'Jazz Funk',
+        'ClassicMetal (80s-90s)', '60s Rock', 'Modern Funk', 'Dark Hybrid', 'Big Room (Layered)',
+        'Fat Rock (Power Toms)', 'Pop-Rock (Studio)',
+        'Dry & Heavy (Folk Rock)', 'Second Line', 'Heavy Metal', 'Arena Stage',
+        'Warmer Funk', 'Super Boom (Layered)', 'Jazz',
+        'Live Rock', 'Shuffle (Blues)', 'Alternative (METAL)', 'Rockin Gate (80s)',
+        'West Coast (FUNK)', 'Live Fusion', 'Speed Metal',
+        'Cassette (Lo-Fi Compress)', 'Bigga Bop (Jazz)', 'Funk Rock',
+        'Alternative (Rock)', 'Tight Prog', 'Unplugged',
+    ])
+    # Just to be explicit about what we're keeping, and what we're leaving out:
+    expected_exclude_kit_names = set([
+        'JingleStacks (2nd Hi-Hat)', 'Raw Dnb (Layered Hybrid)', 'More Cowbell (Pop-Rock)',
+        '808 Simple', '909 Simple', 'Nu RNB', 'Ele-Drum', 'Custom1', 'Custom2', 'Custom3',
+        'Compact Lite (w/ Tambourine HH)', 'Deep Daft',
+    ])
+    exclude_kit_names = all_kit_names - include_kit_names
+
+    print(f'Found {len(all_kit_names)} unique kits:')
+    print(all_kit_names)
+
+    print(f'Keeping {len(include_kit_names)} kits:')
+    print(include_kit_names)
+
+    print(f'Excluding {len(exclude_kit_names)} kits:')
+    print(exclude_kit_names)
+
+    assert(len(exclude_kit_names) == len(expected_exclude_kit_names))
+
+    original_session_row_count = len(metadata_df)
+    metadata_df = metadata_df[metadata_df.kit_name.isin(include_kit_names)]
+    print('')
+    print(f'Saving slimmed dataset of {len(metadata_df)} rows to {SLIM_METADATA_OUT_CSV_PATH}.')
+    print(f'Excluded {original_session_row_count - len(metadata_df)} session rows, and {original_row_count - len(metadata_df)} total rows.')
+    metadata_df.to_csv(SLIM_METADATA_OUT_CSV_PATH, index=False)
